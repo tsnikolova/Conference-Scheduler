b3b2ebf8fe74a1063c610cee6524f8b397ca3b1b
tsnikolova <cvetelina94@gmail.com>
2015-11-21 14:38:07 +0200

    sessions created

---
+++ Framework/Sessions/ISession.php
@@ -0,0 +1,10 @@
+<?php
+namespace Framework\Sessions;
+interface ISession
+{
+    public function getSessionId();
+    public function saveSession();
+    public function destroySession();
+    public function __get($name);
+    public function __set($name, $value);
+}
